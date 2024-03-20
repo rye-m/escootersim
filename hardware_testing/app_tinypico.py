eedf1c79e2643016575a55b7e48832706a240c0c
Rye M <ryem@dhcp-vl2051-1759.eduroam.cornell.edu>
2024-03-20 11:14:14 -0400

    added webapp that can send requesto to Spotify API

--- hardware_testing/app_tinypico.py
+++ hardware_testing/app_tinypico.py
@@ -1,24 +1,43 @@
 import requests
 import json
 import base64
+import time
+import board
+import busio
+import adafruit_mpr121
 
-Client_ID = '978e25ba8b0442adaeca418801d80593'
-Client_secret = 'f9d9bc4ddd8640a5bf3cd8bed05a9db7'
-# token = 'BQCvxE0gLfxHpBDk8NbL8bCCKkgVlYhBwJd7Vz7PONNjcOpQ3nFDv1yOYUpt5AXZK7P-fA-FuYgqIlYOTdA7kZ9pSzUFxNOzZRnsEj0JDa4FQeRcouE'
 
-def get_token():
-    # base64 encording
-    client_info_str = '{}:{}'.format(Client_ID, Client_secret)
-    client_info_byte = client_info_str.encode("ascii")     # get token
-    client_info_base64 = base64.b64encode(client_info_byte) 
-    base64_string = client_info_base64.decode("ascii") 
-    # send request
-    header = {"Authorization": "Basic {}".format(base64_string)}
-    data = {"grant_type": "client_credentials",
-            "scope": "playlist-modify-private user-library-read user-read-currently-playing user-read-playback-state"}
-    response = requests.post(url='https://accounts.spotify.com/api/token', headers=header, data=data)
-    token = response.json()["access_token"]
-    return token
+i2c = busio.I2C(board.SCL, board.SDA)
+mpr121 = adafruit_mpr121.MPR121(i2c)
+
+while True:
+    for i in range(12):
+        if mpr121[i].value:
+            print(f"Twizzler {i} touched!")
+    time.sleep(0.25)  # Small delay to keep from spamming output messages.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
