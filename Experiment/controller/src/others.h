dc3e385a4a8b93bcf2f9f3f513685d8d29957958
Rye M <ryem@dhcp-vl2051-1253.eduroam.cornell.edu>
2024-05-08 10:52:53 -0400

    removed gif files

---
+++ Experiment/controller/src/others.h
@@ -0,0 +1,165 @@
+#include "Adafruit_MPR121.h"
+#include <SparkFun_Qwiic_Button.h>
+#include <seesaw_neopixel.h>
+// #include <network.h>
+
+const int sequenceLength = 10;
+const int n = 1;
+const int timeout = 3000;
+
+QwiicButton button;
+uint8_t brightness = 100;   //The brightness to set the LED to when the button is pushed
+                            //Can be any value between 0 (off) and 255 (max)
+
+
+bool isPressed(int pin, int thr){
+  int deg = analogRead(pin);
+  if (deg > thr) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
+bool playOrPause() {
+
+    int res = sendRequest("api",  "currently-playing");
+    
+    if (res == 201) {
+        Serial.print("play");
+        sendRequest("api",  "play");
+    }
+    else if (res == 202) {
+        Serial.print("pause");
+        sendRequest("api",  "pause");
+    }
+
+  return true;
+}
+
+
+std::vector<int> generateRandomSequence(int length) {
+    std::vector<int> sequence;
+    std::uniform_int_distribution<> dis(1, 9); // Generate integers between 1 and 9
+    
+    Serial.print("sequence: ");
+    for (int i = 0; i < length; ++i) {
+        int temp = rand() % 10;
+        sequence.push_back(temp);
+        Serial.print(temp);
+    }
+      Serial.println("");
+    return sequence;
+}
+
+
+String YesOrNo_button(QwiicButton button){
+  uint32_t  start_time;
+  uint32_t  start_time_pressed;
+  uint32_t  diff;
+
+  start_time = millis();
+  while (true) {
+      if(button.isPressed()){  
+          start_time_pressed = millis();
+          button.LEDon(100);
+          while (button.isPressed()){
+            // Serial.print(".");
+          }
+          button.LEDoff();
+          diff = millis() - start_time_pressed;
+          if(diff <= 500){
+              Serial.println("yes");
+              return "yes";
+          }
+          else{
+              Serial.println("no");
+              return "no";
+          }
+          break;
+      }
+      else{
+          if(millis() - start_time > timeout){
+            return "timeout";
+            break;
+          } 
+      }
+  }            
+}
+
+
+void react(String answer){
+    sendRequest("nback", answer);
+    Serial.println(answer);
+    delay(300);
+}
+
+bool assert_result(int i, std::vector<int> sequence, String userInput){
+
+    if (i - n < 0){
+        if (userInput == "yes"){
+            react("incorrect");
+        }
+        else if (userInput == "no"){
+            react("correct");
+            return true;
+        }
+        else if (userInput == "timeout") {
+            react("timedout");
+            }
+        else {
+            Serial.println("Invalid input! Please enter 'y' or 'n'.");
+        }
+    }
+    else{
+        if (userInput == "yes") {
+            if (sequence[i] == sequence[i - n]) {
+                react("correct");
+                return true;
+            }
+            else {
+                react("incorrect");
+            }
+        }
+        else if (userInput == "no") {
+            if (sequence[i] != sequence[i - n]) {
+                react("correct");
+                return true;
+            }
+            else {
+                react("incorrect");
+            }
+        }
+        else if (userInput == "timeout") {
+            react("timedout");;
+            }
+        else {
+            Serial.println("Invalid input! Please enter 'y' or 'n'.");
+        }
+    }
+    return false;
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+void changeColor(int status, seesaw_NeoPixel pixels) {
+    int color;
+
+    switch (status){
+        case 2:
+          color = 0xEC3440;
+        case 3:
+          color = 0xFDD503;
+        case 4:
+          color = 0x02AFD7;
+        case 5:
+          color = 0x6C2EB9;
+
+    for (uint8_t i=0; i< pixels.numPixels(); i++) {
+        pixels.setPixelColor(0, color);
+        pixels.show();
+    }
+  }
+}
---
+++ Experiment/server/index.js
@@ -0,0 +1,283 @@
+/**
+ * This is an example of a basic node.js script that performs
+ * the Authorization Code oAuth2 flow to authenticate against
+ * the Spotify Accounts.
+ *
+ * For more information, read
+ * https://developer.spotify.com/documentation/web-api/tutorials/code-flow
+ */
+
+var express = require('express');
+var request = require('request');
+var crypto = require('crypto');
+var cors = require('cors');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser')
+
+var client_id = '978e25ba8b0442adaeca418801d80593'; // your clientId
+var client_secret = 'f9d9bc4ddd8640a5bf3cd8bed05a9db7'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+
+var mode = 'none'; // init app mode
+
+global.ACCESS_TOKEN = ''
+
+const generateRandomString = (length) => {
+  return crypto
+  .randomBytes(60)
+  .toString('hex')
+  .slice(0, length);
+}
+
+var stateKey = 'spotify_auth_state';
+
+var app = express();
+app.use(express.static(__dirname + '/public'))
+   .use(cors())
+   .use(cookieParser());
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+var path = require('path');
+
+//Return the index for any other GET request
+app.get('/', function (req, res) {
+    console.log('Hello, guest!');
+    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+});
+
+
+app.get('/mode_tinypico', jsonParser, function(req, res){
+
+  statuscode = 400;
+  switch (mode) {
+    case 'Spotify_button':
+      console.log('Spotify_button');
+      statuscode = 201;
+      break;
+    case 'Spotify_footpedal':
+      console.log('Spotify_footpedal');
+      statuscode = 202;
+      break;
+    case 'Spotify_gearshifter':
+      console.log('Spotify_gearshifter');
+      statuscode = 203;
+      break;
+    case 'Spotify_throttle':
+      console.log('Spotify_throttle');
+      statuscode = 204;
+      break;
+    case 'Nback_button':
+      console.log('Nback_button');
+      statuscode = 205;
+      break;
+    case 'Nback_footpedal':
+      console.log('Nback_footpedal');
+      statuscode = 206;
+      break;
+    case 'Nback_gearshifter':
+      console.log('Nback_gearshifter');
+      statuscode = 207;
+      break;
+    case 'Nback_throttle':
+      console.log('Nback_throttle');
+      statuscode = 208;
+      break;
+    default:
+      console.log(`Sorry, there is no ${mode}.`);
+      break;
+  }
+  
+  res.sendStatus(statuscode);        
+
+})
+
+
+app.get('/mode_server/:mode', jsonParser, function(req, res){
+
+  if (req.params['mode'] != 'mode'){
+    mode = req.params['mode']
+  }
+  console.log('mode: ' + mode);
+
+  if (RegExp("^Spotify").test(mode)){
+    res.sendFile('spotify.html', {root: path.join(__dirname, 'public')});
+  }
+  else{
+    res.set('Content-Type', 'text/html');
+    res.send(JSON.stringify(mode));  }
+
+})
+
+
+app.post('/api', jsonParser, function(req, res){
+
+  command = req.body.command
+
+  var authOptions = {
+    url: 'https://api.spotify.com/v1/me/player/' + command,
+    headers: {
+      'content-type': 'application/json',
+      'Authorization': 'Bearer ' + global.ACCESS_TOKEN,
+    },
+    json: true
+  };
+  console.log("authOptions.url: " + authOptions.url);
+
+  if (command == 'next' || command == 'previous'){
+    request.post(authOptions, function(error, response, body) {
+      res.json(response.statusCode);
+      console.log("response: " + response.statusCode);
+    })
+  }
+  else if (command == 'play' || command == 'pause'){
+    request.put(authOptions, function(error, response, body) {
+      res.json(response.statusCode);
+      console.log("response: " + response.statusCode);
+    })
+  }
+  else if (command == 'currently-playing'){
+    request.get(authOptions, function(error, response, body) {
+      if (response.body.is_playing == false) {
+        res.sendStatus(201);        
+          }
+      else if (response.body.is_playing == true) {
+        res.sendStatus(202);        
+      }
+      else { res.sendStatus(500); }
+      console.log("statusCode: " + response.statusCode);
+      console.log("is_playing: " + response.body.is_playing);
+    })}
+  }
+)
+
+app.post('/nback', jsonParser, function(req, res){
+
+  command = req.body.command
+  console.log("command: " + command);
+
+var player = require('play-sound')();
+player.play('./audio/' + command + '.mp3', (err) => {
+    if (err) console.log(`Could not play sound: ${err}`);
+});
+  res.sendStatus(200);        
+
+})
+
+
+app.get('/login', function(req, res) {
+
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  var scope = 'user-modify-playback-state user-read-private user-read-email user-read-currently-playing';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+
+app.get('/callback', function(req, res) {
+
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code',
+        scope: 'user-read-currently-playing user-read-playback-state'
+
+      },
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        Authorization: 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+        
+        global.ACCESS_TOKEN = body.access_token
+
+        //console.log("ACCESS_TOKEN: "+ ACCESS_TOKEN);
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/#' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
+
+app.get('/refresh_token', function(req, res) {
+
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 
+      'content-type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64')) 
+    },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token,
+          refresh_token = body.refresh_token;
+      res.send({
+        'access_token': access_token,
+        'refresh_token': refresh_token
+      });
+    }
+  });
+});
+
+console.log('Listening on 8888');
+app.listen(8888);
