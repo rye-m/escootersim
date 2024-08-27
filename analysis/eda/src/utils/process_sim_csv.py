e37d94ac3402352f89f3a9c3047c999fb0eeb382
Ilan mandel <im334@cornell.edu>
2024-08-27 15:40:58 -0400

    data munging funcitons

--- analysis/eda/src/utils/process_sim_csv.py
+++ analysis/eda/src/utils/process_sim_csv.py
@@ -115,20 +115,32 @@ class Prototype(Enum):
     WATCH = "Watch"
     CONTROL = "Control"
 
+
 def get_scenario(path):
     path_str = str(path)
     task = next((task for task in Task if task.value in path_str), Task.PRACTICE)
-    prototype = next((proto for proto in Prototype if proto.value in path_str), Prototype.CONTROL)
+    prototype = next(
+        (proto for proto in Prototype if proto.value in path_str), Prototype.CONTROL
+    )
 
     return task, prototype
+
 
 def process_csv(path):
     df = pl.read_csv(path, separator=";")
     task, prototype = get_scenario(path)
-    if task:
-        df = df.with_columns(pl.lit(task.value).alias("Task"))
     if prototype:
         df = df.with_columns(pl.lit(prototype.value).alias("Prototype"))
+    if task:
+        df = df.with_columns(pl.lit(task.value).alias("Task"))
+        if task == Task.NBACK:
+            df = fix_nback_logging(df)
+        if task == Task.SONG:
+            df = fix_song_logging(df, prototype)
+    
+    if task == Task.PRACTICE and prototype == Prototype.CONTROL:
+        df = df.with_columns(pl.lit(None).alias("ws_action"))
+        df = df.with_columns(pl.lit(None).alias("ws_value"))
     return df
 
 def read_trials(path):
@@ -141,18 +153,170 @@ def read_trials(path):
         else:
             df = df.drop(["GameTime"])
         dataframes.append(df)
-    
+
     combined_df = pl.concat(dataframes)
     combined_df = expand_encoded_data(combined_df)
-    combined_df = combined_df.drop(["A escooter Pos", "A escooter velocity", "A escooter Rot", "FrameRate", "FrameRate-XRDevice", "Frame Number"])
+    combined_df = combined_df.drop(
+        [
+            "A escooter Pos",
+            "A escooter velocity",
+            "A escooter Rot",
+            "FrameRate",
+            "FrameRate-XRDevice",
+            "Frame Number",
+        ]
+    )
     combined_df = combined_df.filter(pl.col("ScenarioTime").is_not_null())
 
     return combined_df
 
-def scenario_df(df,task= None, prototype= None):
-    if(task is None):
-        return df.filter(pl.col('Prototype') == prototype.value)
-    if(prototype is None):
-        return df.filter(pl.col('Task') == task.value)
+
+def scenario_df(df, task=None, prototype=None):
+    if task is None:
+        return df.filter(pl.col("Prototype") == prototype.value)
+    if prototype is None:
+        return df.filter(pl.col("Task") == task.value)
     else:
-        return df.filter((pl.col('Task') == task.value) & (pl.col('Prototype') == prototype.value))
+        return df.filter(
+            (pl.col("Task") == task.value) & (pl.col("Prototype") == prototype.value)
+        )
+
+nback_ws_map = {
+    r"\s[0-9]": (
+        "NBACK_DIGIT",
+        pl.col("Websocket_message_action").str.strip_chars(),
+    ),
+    "yes|no": (
+        "NBACK_CLIENT_RESPONSE",
+        pl.col("Websocket_message_action")
+        .str.strip_chars()
+        .replace(["yes", "no"], ["1", "0"]),
+    ),  # yes=1, no=0
+    "Accuracy=": (
+        "NBACK_CLIENT_ACCURACY",
+        pl.col("Websocket_message_action").str.extract(
+            r"=\s*([0-9]+\.?[0-9]*)", 1
+        ),
+    ),
+    "Total=": (
+        "NBACK_CLIENT_TOTAL",
+        pl.col("Websocket_message_action").str.extract(
+            r"=\s*([0-9]+\.?[0-9]*)", 1
+        ),
+    ),
+    "end": ("NBACK_MCU_END", pl.lit(1)),  # end = 1
+    "begin": ("NBACK_MCU_BEGIN", pl.lit(0)),  # start = 0
+    "start_nback": (
+        "NBACK_START_COMMAND_BY_RESEARCHER",
+        pl.lit(0),
+    ),  # start = 0
+    "finish_nback": ("NBACK_END_COMMAND_BY_RESEARCHER", pl.lit(1)),  # end = 1
+    "ping": ("PING", pl.lit(1)),
+    "correct|incorrect": (
+        "NBACK_SERVER_RESPONSE",
+        pl.col("Websocket_message_action")
+        .str.strip_chars()
+        .replace(["correct", "incorrect"], ["1", "0"]),
+    ),
+}
+
+def fix_nback_logging(df, mapping=nback_ws_map):
+    df = df.with_columns(pl.lit(None).alias("ws_action"))
+    df = df.with_columns(pl.lit(None).alias("ws_value"))
+
+    for k, v in mapping.items():
+        df = df.with_columns(
+            [
+                pl.when(pl.col("Websocket_message_action").str.contains(k))
+                .then(pl.lit(v[0]))
+                .otherwise(pl.col("ws_action"))
+                .alias("ws_action"),
+                pl.when(pl.col("Websocket_message_action").str.contains(k))
+                .then(v[1])
+                .otherwise(pl.col("ws_value"))
+                .alias("ws_value"),
+            ]
+        )
+    df = df.with_columns(pl.col("ws_value").str.to_decimal())
+    return df
+
+
+def get_remapping(prototype): 
+    if prototype == Prototype.VOICE:
+        playpause = ("SONG_RESEARCHER_PLAY_PAUSE", None)
+    else:
+        playpause = ("SONG_USER_PLAY_PAUSE", None)
+
+    if prototype in [Prototype.VOICE, Prototype.WATCH, Prototype.PHONE]:
+        play = ("SONG_PLAYPAUSE_ACK", pl.lit(1))
+
+    else:
+        play = ("SONG_NEXT_PREVIOUS", pl.lit(1))
+
+    pause = ("SONG_PLAYPAUSE_ACK", pl.lit(0))
+
+    remapped = {
+        r"\bnext\b": ("SONG_NEXT_PREVIOUS", pl.lit(1)),
+        r"\bprevious\b": ("SONG_NEXT_PREVIOUS", pl.lit(-1)),
+        r"next_failed": ("SONG_OOB", pl.lit(1)),
+        r"previous_failed": ("SONG_OOB", pl.lit(-1)),
+        r"Ping": ("SYSTEM_PING", pl.lit(1)),
+        r"next_failed": ("SONG_OOB", pl.lit(1)),
+        r"previous_failed": ("SONG_OOB", pl.lit(-1)),
+        r"playpause": playpause,
+        r"\bplay\b": play,
+    }
+    return remapped
+
+def fix_song_logging(df, prototype):
+    mapping1 = get_remapping(prototype)
+    df = df.with_columns(pl.lit(None).alias("ws_action"))
+    df = df.with_columns(pl.lit(None).alias("ws_value"))
+    for k, v in mapping1.items():
+        if prototype in [Prototype.VOICE, Prototype.PHONE, Prototype.WATCH] and k == r"playpause":
+            df = df.with_columns(
+                [
+                    pl.when(pl.col("Websocket_message_action").str.contains(k))
+                    .then(pl.lit(v[0]))
+                    .otherwise(pl.col("ws_action"))
+                    .alias("ws_action"),
+                    pl.when(pl.col("Websocket_message_action").str.contains(k))
+                    .then(
+                        pl.when(
+                            pl.col("Websocket_message_action")
+                            .eq(k)
+                            .cum_count()
+                            .over("Websocket_message_action")
+                            == 1
+                        )
+                        .then(pl.lit(1))
+                        .when(
+                            pl.col("Websocket_message_action")
+                            .eq(k)
+                            .cum_count()
+                            .reverse()
+                            .over("Websocket_message_action")
+                            == 1
+                        )
+                        .then(pl.lit(-1))
+                        .otherwise(pl.col("ws_value"))
+                    )
+                    .otherwise(pl.col("ws_value"))
+                    .alias("ws_value"),
+                ]
+        )
+        elif k != r"playpause":
+            df = df.with_columns(
+                [
+                    pl.when(pl.col("Websocket_message_action").str.contains(k))
+                    .then(pl.lit(v[0]))
+                    .otherwise(pl.col("ws_action"))
+                    .alias("ws_action"),
+                    pl.when(pl.col("Websocket_message_action").str.contains(k))
+                    .then(v[1])
+                    .otherwise(pl.col("ws_value"))
+                    .alias("ws_value"),
+                ]
+            )
+    df = df.with_columns(pl.col("ws_value").cast(pl.Decimal(precision=None, scale=0)))
+    return df
